2557e38259ef3b86ef9420e1013a37daffb69ba1
ujju <ujjavalgreat@gmail.com>
2025-10-27 11:29:26 -0400

    updated app.py

--- streamlit.sh
+++ streamlit.sh
@@ -2,4 +2,4 @@
 cd /home/site/wwwroot
 pip install --upgrade pip
 pip install -r requirements.txt --no-cache-dir
-python -m streamlit run app.py --server.port 8000 --server.address 0.0.0.0 --server.headless true
+python3 -m streamlit run app.py --server.port 8000 --server.address 0.0.0.0 --server.headless true
